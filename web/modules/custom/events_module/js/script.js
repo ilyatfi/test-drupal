ce481bd3f02370dd8018598ee537cfd31cef6b36
Ilja <ilja.trofimovs@esynergy.lv>
2024-08-07 16:55:55 +0300

    datepicker js library added and adapted

---
+++ web/modules/custom/events_module/js/script.js
@@ -0,0 +1,12 @@
+const min = new easepick.create({
+  element: document.getElementById('edit-field-date-value-1-min'),
+  css: [
+    'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css',
+  ],
+});
+const max = new easepick.create({
+    element: document.getElementById('edit-field-date-value-1-max'),
+    css: [
+      'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css',
+    ],
+  });
