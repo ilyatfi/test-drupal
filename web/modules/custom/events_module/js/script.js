30aac647a81e33ae5e6a9d1d0b415692cb01d683
Ilja <ilja.trofimovs@esynergy.lv>
2024-08-08 10:30:29 +0300

    beautiful and functional datepicker

--- web/modules/custom/events_module/js/script.js
+++ web/modules/custom/events_module/js/script.js
@@ -1,12 +1,11 @@
-const min = new easepick.create({
-  element: document.getElementById('edit-field-date-value-1-min'),
-  css: [
-    'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css',
-  ],
-});
-const max = new easepick.create({
-    element: document.getElementById('edit-field-date-value-1-max'),
+const picker = new easepick.create({
+    element: '#edit-field-date-value-1',
     css: [
-      'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css',
+        'https://cdn.jsdelivr.net/npm/@easepick/core@1.2.1/dist/index.css',
+        'https://cdn.jsdelivr.net/npm/@easepick/range-plugin@1.2.1/dist/index.css',
     ],
-  });
+    plugins: ['RangePlugin'],
+    RangePlugin: {
+        tooltip: true,
+    },
+});
