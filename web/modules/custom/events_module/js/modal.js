0e1e4ab227f3401a664739a39375124508320d12
Ilja <ilja.trofimovs@esynergy.lv>
2024-08-27 18:21:17 +0300

    modal js made with drupal behavior

--- web/modules/custom/events_module/js/modal.js
+++ web/modules/custom/events_module/js/modal.js
@@ -1,25 +1,35 @@
-// Get the modal
-var modal = document.getElementsByClassName("myModal")[0];
+(function (Drupal) {
 
-// Get the button that opens the modal
-var btn = document.getElementsByClassName("myBtn")[0];
+  Drupal.behaviors.modalBehaviour = {
+ 
+    attach: (context, settings) => {
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+      // Get the modal
+      let modal = document.getElementsByClassName("myModal")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+      // Get the button that opens the modal
+      let btn = document.getElementsByClassName("myBtn")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+      // Get the <span> element that closes the modal
+      let span = document.getElementsByClassName("close")[0];
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+      // When the user clicks on <span> (x), close the modal
+      btn.addEventListener("click", () => {
+        modal.style.display = "block";
+      });
+      
+      // When the user clicks anywhere outside of the modal, close it
+      span.addEventListener("click", () => {
+        modal.style.display = "none";
+      });
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+    },
+  };
+
+})(Drupal);
